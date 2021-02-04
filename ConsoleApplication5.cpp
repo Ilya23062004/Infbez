74c8041de28609e47c13e8359103bb04de7e65fe
Ilya23062004 <71217869+Ilya23062004@users.noreply.github.com>
2021-02-04 15:47:02 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string s = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+	string b = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+	string str;
+	string str1 = " ";
+
+	int sdvig, nums, numb;
+
+	cout << "Введите строку ";
+	getline(cin, str);
+	cout << "Введите сдвиг ";
+	cin >> sdvig;
+
+	for (int i = 0; i < str.size(); i++) {
+
+		nums = s.find(str[i]);
+		numb = b.find(str[i]);
+
+	/*	if (str[i] == ' ')
+		{
+			cout << " ";
+			continue;
+		}
+
+		if (str[i] == '!')
+		{
+			cout << "!";
+			continue;
+		}
+
+		if (str[i] == ',')
+		{
+			cout << ",";
+			continue;
+		}
+
+		if (str[i] == '.')
+		{
+			cout << ".";
+			continue;
+		}
+
+		if (str[i] == '?')
+		{
+			cout << "?";
+			continue;
+		}
+
+		if (str[i] == ':')
+		{
+			cout << ":";
+			continue;
+		}
+
+		if (str[i] == ' " ')
+		{
+			cout << str[i];
+			continue;
+		}*/
+		if (nums == -1 && numb == -1) {
+			str1.push_back(str[i]);
+		}
+
+			if (numb + sdvig <= 32 and numb != -1) {
+				str1.push_back(b[numb + sdvig]);
+			}
+			if (numb + sdvig > 32 and numb != -1) {
+				str1.push_back(b[numb + sdvig - 33]);
+			}
+			if (nums + sdvig <= 32 and nums != -1) {
+				str1.push_back(s[nums + sdvig]);
+			}
+			
+			if (nums + sdvig > 32 and nums != -1) {
+				str1.push_back(s[nums + sdvig - 33]);
+			}
+			
+	}
+
+	cout << str1;
+
+	return 0;
+}
---
+++ цезарь3.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+
+
+	/*for (int i = 0;i <= 255;i++)
+	{
+		cout << "code = " << i << " " << "char = " << char(i) << endl;
+	}
+
+}*/
+	int sdvig;
+	string str = " ";
+	getline(cin, str);
+	cin >> sdvig;
+
+	for (int i = 0; i < str.length(); i++) {
+
+		int l = int(unsigned char(str[i])) + sdvig;
+
+		if (str[i] == ' ')
+		{
+			cout << " ";
+			continue;
+		}
+
+		if (str[i] == '!')
+		{
+			cout << "!";
+			continue;
+		}
+
+		if (str[i] == ',')
+		{
+			cout << ",";
+			continue;
+		}
+
+		if (str[i] == '.')
+		{
+			cout << ".";
+			continue;
+		}
+
+		if (str[i] == '?')
+		{
+			cout << "?";
+			continue;
+		}
+
+		if (sdvig >= 33)
+		{
+			while (sdvig >= 33) sdvig = sdvig - 33;
+
+		}
+		/*if (l > 223)
+		{
+			while (l > 223)
+			{
+				l = l - 32;
+			}
+			cout << char(l);
+		}*/
+
+		if (l > 255)
+		{
+			l = l - 32;
+
+			cout << unsigned char(l);
+		}
+
+
+		cout << unsigned char(l);
+	}
+
+}
